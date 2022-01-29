7dfbffdba24b18daac53d143d69a6b730ece8c57
Mao-maitre-du-Chaos <mohamedabdillahiomar123@gmail.com>
2022-01-29 08:03:04 +0300

    Pour le professeur Wojcieh

---
+++ index.f
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>CV</title>
+
+
+</head>
+<body>
+<div id="bloc1" style="font-weight:bold;background-color:red;">
+Me concernant:
+</div>
+<div style="font-weight:bold;background-color:green;">
+  Nom:
+</div>
+<span>
+Mohamed Bouh
+</span>
+<div style="font-weight:bold;background-color:green;">
+  Mon adresse:
+</div>
+<span>
+24 Boulevard d'Amsterdam,appartement 3
+</span>
+<div style="font-weight:bold;background-color:green;">
+ Mes objectifs:
+</div>
+<div>
+  Professionnel dynamique et soucieux du détail cherchant à tirer parti de ses compétences techniques et de ses solides compétences interpersonnelles pour fournir un service client de haute qualité
+
+</div>
+<div>
+   Excellente écoute des besoins des clients et dépannage sur place pour résoudre les problèmes rapidement
+</div>
+<div>
+   Développer et entretenir des relations avec les clients pour assurer la satisfaction des clients
+</div>
+<div>
+   Dédié et fiable
+</div>
+<div>
+   Attitude positive
+</div>
+<div style="font-weight:bold;background-color:green;">
+ Mes compétences:
+</div>
+<div>
+   Communication claire et efficace avec les clients et les collègues
+</div>
+<div>
+  Capacité à effectuer plusieurs tâches dans différents environnements
+</div>
+   Grand souci du détail
+</div>
+<div>
+   Saisie précise des données et tenue des dossiers
+</div>
+<div>
+   Calme sous pression
+</div>
+<div>
+   Resolution de problèmes
+</div>
+<div style="font-weight:bold;background-color:red;">
+Experiences:
+</div>
+<div>
+  Volentaire a "Heathrington Call Center"
+</div>
+<div style="font-weight:bold;background-color:green;">
+  Education:
+</div>
+<span style="font-weight:bold;">
+Ecole Internationale Espoir:
+</span>
+<span>
+Diplome D'Exetat
+</span>
+<div style="font-weight:bold;background-color:red;">
+  References
+</div>
+<div>
+À la demande
+</div>
+<div>
+      <a href="#bloc1">Aller vers le debut de la page...</a>
+    </div>
+
+ </body>
+</html>
